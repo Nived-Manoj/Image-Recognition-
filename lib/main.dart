e9d7c7eff30e26017039d9ce3cabcaae649a1cbb
Nived-Manoj <nivedmanoj001@gmail.com>
2025-01-13 16:07:47 +0530

    final committ

--- lib/main.dart
+++ lib/main.dart
@@ -174,7 +174,7 @@ class _ModelTestScreenState extends State<ModelTestScreen> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _pickImage,
-              child: Text(' aPick Image and Run Inference'),
+              child: Text('Pick Image and Run Inference'),
             ),
           ],
         ),
